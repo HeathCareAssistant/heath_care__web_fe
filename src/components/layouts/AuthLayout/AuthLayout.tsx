9d656e278357b6e2b6043b2afa99e17953dc855d
minhnhat166 <minhnhatphan2003@gmail.com>
2025-02-09 22:25:19 +0700

    feat: update AuthLayout and routes to use modern layout; adjust loading fallback style

--- src/components/layouts/AuthLayout/AuthLayout.tsx
+++ src/components/layouts/AuthLayout/AuthLayout.tsx
@@ -1,6 +1,5 @@
 import {
-    LAYOUT_TYPE_BLANK,
-    LAYOUT_TYPE_SIMPLE
+    LAYOUT_TYPE_BLANK
 } from '@/constants/theme.constant'
 import { useAppSelector } from '@/store'
 import View from '@/views'
@@ -8,10 +7,11 @@ import Side from './Side'
 
 const AuthLayout = () => {
     const layoutType = useAppSelector((state) => state.theme.layout.type)
+    console.log("🚀 ~ AuthLayout ~ layoutType:", layoutType)
 
     return (
         <div className="app-layout-blank flex flex-auto flex-col h-[100vh]">
-            {layoutType === LAYOUT_TYPE_BLANK || LAYOUT_TYPE_SIMPLE ? (
+            {layoutType === LAYOUT_TYPE_BLANK ? (
                 <View />
             ) : (
                 <Side>
--- src/configs/routes.config/authRoute.tsx
+++ src/configs/routes.config/authRoute.tsx
@@ -7,24 +7,28 @@ const authRoute: Routes = [
         path: `/sign-in`,
         component: lazy(() => import('@/views/auth/SignIn')),
         authority: [],
+        meta: { layout: 'modern' },
     },
     {
         key: 'signUp',
         path: `/sign-up`,
         component: lazy(() => import('@/views/auth/SignUp')),
         authority: [],
+        meta: { layout: 'modern' },
     },
     {
         key: 'forgotPassword',
         path: `/forgot-password`,
         component: lazy(() => import('@/views/auth/ForgotPassword')),
         authority: [],
+        meta: { layout: 'modern' },
     },
     {
         key: 'resetPassword',
         path: `/reset-password`,
         component: lazy(() => import('@/views/auth/ResetPassword')),
         authority: [],
+        meta: { layout: 'modern' },
     },
 ]
 
