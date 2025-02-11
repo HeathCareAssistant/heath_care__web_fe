f96252b14476e51015a16a6b63a64f2ee864d42b
minhnhat166 <minhnhatphan2003@gmail.com>
2025-02-11 16:14:06 +0700

    feat: refactor layout component to improve authentication handling and loading state; enhance layout type management

--- src/components/layouts/Layouts.tsx
+++ src/components/layouts/Layouts.tsx
@@ -1,17 +1,17 @@
-import { useMemo, lazy, Suspense } from 'react'
 import Loading from '@/components/shared/Loading'
-import { useAppSelector } from '@/store'
 import {
+    LAYOUT_TYPE_BLANK,
     LAYOUT_TYPE_CLASSIC,
+    LAYOUT_TYPE_DECKED,
     LAYOUT_TYPE_MODERN,
     LAYOUT_TYPE_SIMPLE,
     LAYOUT_TYPE_STACKED_SIDE,
-    LAYOUT_TYPE_DECKED,
-    LAYOUT_TYPE_BLANK,
 } from '@/constants/theme.constant'
+import { useAppSelector } from '@/store'
 import useAuth from '@/utils/hooks/useAuth'
 import useDirection from '@/utils/hooks/useDirection'
 import useLocale from '@/utils/hooks/useLocale'
+import { lazy, Suspense, useMemo } from 'react'
 
 const layouts = {
     [LAYOUT_TYPE_CLASSIC]: lazy(() => import('./ClassicLayout')),
@@ -22,28 +22,36 @@ const layouts = {
     [LAYOUT_TYPE_BLANK]: lazy(() => import('./BlankLayout')),
 }
 
+const AuthLayout = lazy(() => import('./AuthLayout'))
+
 const Layout = () => {
     const layoutType = useAppSelector((state) => state.theme.layout.type)
-
     const { authenticated } = useAuth()
 
     useDirection()
-
     useLocale()
 
     const AppLayout = useMemo(() => {
+        // if (
+        //     layoutType === LAYOUT_TYPE_BLANK ||
+        //     layoutType === LAYOUT_TYPE_MODERN
+        // ) {
+        //     return layouts[layoutType]
+        // }
+
         if (!authenticated && layoutType === LAYOUT_TYPE_SIMPLE) {
-            return layouts[LAYOUT_TYPE_SIMPLE]
+            return layouts[layoutType]
         }
-        return authenticated
-            ? layouts[layoutType]
-            : lazy(() => import('./AuthLayout'))
+
+        return authenticated ? layouts[layoutType] : AuthLayout
     }, [layoutType, authenticated])
 
     return (
         <Suspense
             fallback={
-                <div className="flex flex-auto flex-col h-screen">
+                <div className="flex flex-auto flex-col h-screen items-center justify-center">
+                    {' '}
+                    {/* Center the loading */}
                     <Loading loading={true} />
                 </div>
             }
